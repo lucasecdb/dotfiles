c8e8ffe8fd5feb56cb76e79825d1fd5c81a32655
Lucas Cordeiro <ecdb.lucas@gmail.com>
2015-11-15 01:06:53 -0300

    added install.sh

---
+++ vim/bundle/install.sh
@@ -0,0 +1,17 @@
+# powerline-fonts
+git clone https://github.com/powerline/fonts.git
+
+# nerdtree
+git clone https://github.com/scrooloose/nerdtree.git
+
+# numbers
+git clone https://github.com/myusuf3/numbers.vim.git
+
+# syntastic
+git clone https://github.com/scrooloose/syntastic.git
+
+# airline
+git clone https://github.com/bling/vim-airline
+
+# fugitive
+git clone git://github.com/tpope/vim-fugitive.git
