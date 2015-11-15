6d5365b70b6984e20e7f63c6478b0c4ee010e92d
Lucas Cordeiro <ecdb.lucas@gmail.com>
2015-11-15 01:46:57 -0300

    added fancy installation

--- vim/bundle/install.sh
+++ vim/bundle/install.sh
@@ -1,17 +1,58 @@
+echo 'Installing plugins...'
+echo ''
+
 # powerline-fonts
-git clone https://github.com/powerline/fonts.git
+echo 'Installing FONTS'
+
+if [[ -f ./fonts/install.sh ]]; then
+	echo 'FONTS already installed, skipping...'
+else
+	git clone https://github.com/powerline/fonts.git
+fi
 
 # nerdtree
-git clone https://github.com/scrooloose/nerdtree.git
+echo 'Installing NERDTREE'
+
+if [[ -f ./nerdtree/README.markdown ]]; then
+	echo 'NERDTREE already installed, skipping...'
+else
+	git clone https://github.com/scrooloose/nerdtree.git
+fi
 
 # numbers
-git clone https://github.com/myusuf3/numbers.vim.git
+echo 'Installing NUMBERS'
+
+if [[ -f ./numbers/README.md ]]; then
+	echo 'NUMBERS already installed, skipping...'
+else
+	git clone https://github.com/myusuf3/numbers.vim.git ./numbers
+fi
 
 # syntastic
-git clone https://github.com/scrooloose/syntastic.git
+echo 'Installing SYNTASTIC'
+
+if [[ -f ./syntastic/README.markdown ]]; then
+	echo 'SYNTASTIC already installed, skipping...'
+else
+	git clone https://github.com/scrooloose/syntastic.git
+fi
 
 # airline
-git clone https://github.com/bling/vim-airline
+echo 'Installing AIRLINE'
+
+if [[ -f ./vim-airline/README.md ]]; then
+	echo 'AIRLINE already installed, skipping...'
+else
+	git clone https://github.com/bling/vim-airline
+fi
 
 # fugitive
-git clone git://github.com/tpope/vim-fugitive.git
+echo 'Installing FUGITIVE'
+
+if [[ -f ./vim-fugivite/README.markdown ]]; then
+	echo 'FUGITIVE already installed, skipping...'
+else
+	git clone git://github.com/tpope/vim-fugitive.git
+fi
+
+echo 'Installation done! :-)'
