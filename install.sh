a307316c4492921dc6ddfcc5cb245681ae5feb6a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2015-11-15 15:41:51 -0300

    updated script

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/zsh
 
-if [ $(pwd) == /home/$USER/.dotfiles ]; then
-	# make symbolic links
-	
-	# vimrc
-	ln -s vimrc ~/.vimrc
-
-	# tmux.conf
-	ln -s tmux.conf ~/.tmux.conf
-
-	# zshrc
-	ln -s zshrc ~/.zshrc
+if [ $(pwd) != /home/$USER/.dotfiles ]; then
+	cd
+	cd .dotfiles
 fi
+
+# make symbolic links
+
+# vimrc
+ln -s vimrc ~/.vimrc
+
+# tmux.conf
+ln -s tmux.conf ~/.tmux.conf
+
+# zshrc
+ln -s zshrc ~/.zshrc
+
+# bashrc
+ln -s bashrc ~/.bashrc
