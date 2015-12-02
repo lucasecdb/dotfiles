b159afe708462be029d519bd2ca42628ee3b45ec
Lucas Cordeiro <ecdb.lucas@gmail.com>
2015-12-02 16:31:58 -0300

    updated script for more stability

--- install.sh
+++ install.sh
@@ -17,7 +17,9 @@ if [[ -f ~/.vimrc ]]; then
 fi
 
 echo 'Creating symlink for vimrc'
-ln -s ./vim/vimrc ~/.vimrc
+ln -s /home/$USER/.dotfiles/vim/vimrc ~/.vimrc
+
+echo ''
 
 # tmux.conf
 if [[ -f ~/.tmux.conf ]]; then
@@ -28,7 +30,9 @@ if [[ -f ~/.tmux.conf ]]; then
 fi
 
 echo 'Creating symlink for tmux.conf'
-ln -s ./tmux/tmux.conf ~/.tmux.conf
+ln -s /home/$USER/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+
+echo ''
 
 # zshrc
 if [[ -f ~/.zshrc ]]; then
@@ -39,7 +43,9 @@ if [[ -f ~/.zshrc ]]; then
 fi
 
 echo 'Creating symlink for zshrc'
-ln -s ./zsh/zshrc ~/.zshrc
+ln -s /home/$USER/.dotfiles/zsh/zshrc ~/.zshrc
+
+echo ''
 
 # bashrc
 if [[ -f ~/.bashrc ]]; then
@@ -50,26 +56,44 @@ if [[ -f ~/.bashrc ]]; then
 fi
 
 echo 'Creating symlink for bashrc'
-ln -s ./bash/bashrc ~/.bashrc
+ln -s /home/$USER/.dotfiles/bash/bashrc ~/.bashrc
+
+echo ''
+
+# cd to home directory
+
+cd
 
 # i3
-if [[ -f ~/.i3 ]]; then
+if [[ -f .i3 ]]; then
 	echo 'Backing up ~/.i3.conf'
 
-	cp ~/.i3 ~/.i3.orig
-	rm ~/.i3
+	cp .i3 .i3.orig
+	rm .i3
 fi
 
 echo 'Creating symlink for i3'
-ln -s ./i3 ~/.i3
+ln -s /home/$USER/.dotfiles/i3 /home/$USER/.i3
+
+if [[ -f .dotfiles/i3/i3/config ]]; then
+	echo 'Cleaning up extra files'
+	rm .dotfiles/i3/i3
+fi
+
+echo ''
 
 # fonts
-if [[ -f ~/.fonts ]]; then
+if [[ -f .fonts ]]; then
 	echo 'Backing up ~/.fonts'
 
-	cp ~/.fonts ~/.fonts.orig
-	rm ~/.fonts
+	cp .fonts .fonts.orig
+	rm .fonts
 fi
 
 echo 'Creating symlink for fonts'
-ln -s ./fonts ~/.fonts
+ln -s /home/$USER/.dotfiles/fonts /home/$USER/.fonts
+
+if [[ -f .dotfiles/fonts/fonts/Hack-Regular.ttf ]]; then
+	echo 'Cleaning up extra files'
+	rm .dotfiles/fonts/fonts
+fi
