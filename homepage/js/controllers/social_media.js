f784201f4635b8bf843e997605d18aca36160538
Lucas Cordeiro <ecdb.lucas@gmail.com>
2016-03-16 21:48:46 -0300

    Transcripted homepage to use angularjs and dinamicaly generate the sites

---
+++ homepage/js/controllers/social_media.js
@@ -0,0 +1,28 @@
+app.controller("social_media", ["$scope", function ($scope) {
+	$scope.sites = [
+		{
+			"name": "Twitter",
+			"link": "https://twitter.com"
+		},
+		{
+			"name": "Reddit",
+			"link": "www.reddit.com"
+		},
+		{
+			"name": "Youtube",
+			"link": "https://www.youtube.com"
+		},
+		{
+			"name": "Gmail",
+			"link": "https://gmail.com"
+		},
+		{
+			"name": "Facebook",
+			"link": "https://www.facebook.com"
+		},
+		{
+			"name": "Habitica",
+			"link": "https://habitica.com/#/tasks"
+		}
+	];
+}]);
